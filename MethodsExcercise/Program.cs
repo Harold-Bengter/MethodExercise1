ac68c9d58ff2b65c7a3bf4be439b79ddfe5fc49f
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-05 12:03:10 -0400

    Finised Exercise

---
+++ MethodsExcercise/Program.cs
@@ -0,0 +1,24 @@
+﻿//Excerise 1
+
+
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+
+Console.WriteLine("What is your favorite color?");
+var color = Console.ReadLine();
+
+Console.WriteLine("What is your favorite band?");
+var band = Console.ReadLine();
+
+Console.WriteLine("What is your favorite number?");
+var number = Console.ReadLine();
+
+Console.WriteLine("What is your favorite animal?");
+var animal = Console.ReadLine();
+
+Console.WriteLine($"Greetings,{name}" +
+    $"I cant belive you like the color {color}, So do I " +
+    $"When you told me your favorite band was {band} I couldnt help but judge" +
+    $"I've only seen {band} about {number} times" +
+    $"I wish I had a {animal} that would make my days alot better");
+
